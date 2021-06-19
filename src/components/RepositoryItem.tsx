d411886a3a41688abd6360c1f4b7ba4f1932069b
José Augusto <joseaugusto0408@gmail.com>
2021-06-18 23:15:15 -0300

    components typescript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,5 +1,11 @@
-
-export function RepositoryItem(props) {
+interface RepositoryItemProps{
+  repository:{
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+export function RepositoryItem(props: RepositoryItemProps) {
   
   return (
     <>
